9a59a9e23fdefb5d3a3e1253a9ff12f732fe4c46
Frederic.F <fredericferrera@yahoo.fr>
2021-11-26 12:13:22 +0100

    create a folder for each api based on the docker image challenge

---
+++ stockLstmModel/stockLstmModel/data.py
@@ -0,0 +1,14 @@
+from google.cloud import storage
+
+
+def get_stock_data():
+    """Function to get the data from\
+       the stocksapi table in the database"""
+    pass
+
+def upload_LSTM_prediction():
+    """Function to upload the LSTM prediction\
+        in the database\
+        in predictionmodel table\
+        in the stock_api_pred column"""
+    pass
---
+++ stockSentimentModel/stockSentimentModel/data.py
@@ -0,0 +1,14 @@
+from google.cloud import storage
+
+
+def get_stock_data():
+    """Function to get the data from\
+       the predictionmodel table in the database"""
+    pass
+
+def upload_stockprice_prediction():
+    """Function to upload the stock prices with the sentiment\
+        in the database\
+        in predictionmodel table\
+        in stockprice_prediction column"""
+    pass
